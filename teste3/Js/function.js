9ca9f99132264d61d954129bc4dc53f6e3b1f74e
luan marques <luan6575@gmail.com>
2020-01-10 16:46:38 -0300

    segundo commit

---
+++ teste3/Js/function.js
@@ -0,0 +1,16 @@
+$(function(){
+
+  alert("olá mundo");
+  
+ });
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+   
---
+++ teste3/Js/slides.js
@@ -0,0 +1,17 @@
+$(function(){
+	
+$('.section-slide .container-slide').slick({
+    autoplaySpeed: 2000,
+    autoplay:true,
+    centerMode:false,
+		slidesToShow:1,
+		infinite: true,  
+    arrows:false,
+    dots:false,
+});
+
+
+
+
+
+});
